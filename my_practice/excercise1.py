73e9237299078d62e29dce06839ceeb018fbb841
ShaliniJha1999 <jhashalini1999@gmail.com>
2021-01-28 20:52:01 +0530

    For Learning

--- my_practice/excercise1.py
+++ my_practice/excercise1.py
@@ -2,4 +2,4 @@ name=input("Enter your name:--\n")
 age=int(input("Enter your age:--\n"))
 
 
-print( f"{name}, You will turn 100 in the year {2018 - age +100}")
+print( f"{name}, You will turn 100 in the year {2018 - age+100}")
